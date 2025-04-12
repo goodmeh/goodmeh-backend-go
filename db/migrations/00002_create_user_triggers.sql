4a66ae831984c83d9a3174740a17deea031e99ea
Zhekai <limzhekai1999@live.com>
2025-04-12 18:24:32 +0800

    Fix migrations

--- db/migrations/00002_create_user_triggers.sql
+++ db/migrations/00002_create_user_triggers.sql
@@ -1,13 +1,5 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE FUNCTION on_user_change() RETURNS TRIGGER AS $$ BEGIN NEW.score := NEW.review_count * 10 + NEW.long_review_count * 10 + NEW.photo_count * 5 + NEW.rating_count;
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER user_change_trigger BEFORE
-INSERT
-    OR
-UPDATE ON "user" FOR EACH ROW EXECUTE FUNCTION on_user_change();
 CREATE FUNCTION on_review_change() RETURNS TRIGGER AS $$ BEGIN IF TG_OP = 'INSERT'
 AND LENGTH(NEW.text) > 200 THEN
 UPDATE "user"
@@ -37,6 +29,4 @@ UPDATE ON review FOR EACH ROW EXECUTE FUNCTION on_review_change();
 -- +goose StatementBegin
 DROP TRIGGER review_change_trigger ON review;
 DROP FUNCTION on_review_change();
-DROP TRIGGER user_change_trigger ON "user";
-DROP FUNCTION on_user_change();
 -- +goose StatementEnd
